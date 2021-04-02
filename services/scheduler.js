2b82d4a3f97c914a0b28c4955666c38269229371
Jeremiah Valero <jeremiah.v@scopicsoftware.com>
2021-04-03 01:13:02 +0800

    Implemented multiple jobs and stopping of jobs using api

--- services/scheduler.js
+++ services/scheduler.js
@@ -4,14 +4,13 @@ const binance = require("./binance");
 const schedule = require('node-schedule');
 var INTERVAL_ID;
 var running = false;
-var havePosition = false;
+var positions = [];
 var runningJobs = [];
 module.exports = {
   start: (order) => {
       if(runningJobs.find(job=>job.name === order.symbol) == undefined){
         console.log(`Starting Scheduler for ${order.symbol} at ${order.quantity} quantity`);
-        running = true;
-        var job = schedule.scheduleJob(order.symbol,'0/30 * * * *', validatorJob());
+        var job = schedule.scheduleJob(order.symbol,'0/30 * * * *', async () =>{validatorJob(order)});
         runningJobs.push(job);
         return `Starting Scheduler for ${order.symbol} at ${order.quantity} quantity`;
       }else{
@@ -19,31 +18,41 @@ module.exports = {
       }
   },
   stop: async (order) => {
-    var job = runningJobs.find(job=>job.name === order.symbol);
-    running = false;
-    havePosition = false;
-    console.log(order)
-    var result = await binance.BinanceSell(order);
-    return "stopped scheduler";
+    var runningJob = runningJobs.find(job=>job.name === order.symbol);
+    if(runningJob === undefined){
+     return "No job for this symbol";
+    }
+    runningJob.cancel();
+    runningJobs = runningJobs.filter(job => job.name !== order.symbol);
+    if(positions.find(position => position.symbol === order.symbol) != undefined){
+      var result = await binance.BinanceSell(order);
+      console.log(`Stopped and sold ${order.symbol}`);
+      return "Sold remaining position, stopped scheduler";
+    }else{
+      return "No position, stopped scheduler";
+    }
   },
 };
-async function validatorJob(){
-  if(!havePosition){
-    console.log("Running buy validators")
+async function validatorJob(order){
+  
+  if(positions.find(position => position.symbol === order.symbol) == undefined){
+    console.log(`Running buy validators for ${order.symbol}`);
     if(await validator.ValidateBuy(order.symbol)){
         console.log("Buying");
         var result = await binance.BinanceBuy(order);
         if(result.status ==="FILLED"){
-          havePosition = true;
+        var newPosition = {symbol:order.symbol};
+        positions.push(newPosition);
         }
     }
   }else{
-    console.log("Running sell validators")
+    console.log(`Running sell validators ${order.symbol}`)
     if(await validator.ValidateSell(order.symbol)){
         console.log("Selling");
+        order.quantity = order.quantity * 0.998;//Take fees into consideration.
         var result = await binance.BinanceSell(order);
         if(result.status ==="FILLED"){
-          havePosition = false;
+        positions = positions.filter(position => position.symbol != order.symbol);
         }
     }
   }
